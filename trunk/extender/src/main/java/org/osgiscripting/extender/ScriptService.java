35babe188a6fdb53fc744104d9eb3e854f284fd5
rinswind <rinswind@0f8d3fab-adca-1338-e491-4504548743ed>
2010-06-07 20:15:37 +0000

    Initial commit

---
+++ trunk/extender/src/main/java/org/osgiscripting/extender/ScriptService.java
@@ -0,0 +1,10 @@
+package org.osgiscripting.extender;
+
+/**
+ * Test service
+ * 
+ * @author rinswind
+ */
+public interface ScriptService {
+  Object run(Object  arg);
+}
---
+++ trunk/rubytest/src/main/ruby/Trail.rb
@@ -0,0 +1,19 @@
+require 'java'
+
+module Org
+  module Osgiscripting
+    module Rubytest
+      # Instrumented test implementation of a backup service in JRuby. Used to test
+      # that core module works with JRuby scripts for plugin modules.
+      class TrailRubySample 
+        include org.osgiscripting.extender.ScriptService;
+      
+        def run(arg) 
+          return "result from jruby run Trial with value "+arg+"."
+        end
+      end # class
+    end
+  end  
+end
+
+return Org::Osgiscripting::Rubytest::TrailRubySample.new
